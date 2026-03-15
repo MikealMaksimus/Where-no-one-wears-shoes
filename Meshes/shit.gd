ddd1a563f3f4fd4218decdf7b47ebf11ff706336
MikealMaksimus <aapo.roine@ksyk.fi>
2026-03-15 14:31:04 +0200

    dog

--- Meshes/shit.gd
+++ Meshes/shit.gd
@@ -7,10 +7,11 @@ func _on_inside_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		$Door.position.y = -0.895
 		$Door.show()
-		$Yap.start()
+		await get_tree().create_timer(0.3).timeout
+		$AudioStreamPlayer.play()
 
 
-func _on_yap_timeout() -> void:
+func _on_audio_stream_player_finished() -> void:
 		await $Door/VisibleOnScreenNotifier3D.screen_exited
 		$Door.position.y += 15
 		$Door.hide()
--- guard.gd
+++ guard.gd
@@ -2,3 +2,15 @@ extends Node3D
 
 func interact():
 	$AudioStreamPlayer3D.play()
+
+
+
+func _on_world_dawned() -> void:
+	show()
+	position.y = 1.332
+
+
+func _on_cycler_timeout() -> void:
+	if Info.time == "night":
+		hide()
+		position.y = -1.332
